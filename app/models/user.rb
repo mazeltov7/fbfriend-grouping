a1d32acabc92d14471c1d35ddf2222ffa5d5c765
Yuki Ishikawa <ishikawayuki2@gmail.com>
2017-04-10 11:08:14 +0900

    add devise:views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  devise :rememberable, :trackable, :omniauthable
 
   def self.find_for_oauth(auth)
-    user = User.where(uid: auth.uid, provider: auth.provider).first
+    user = User.where(uid: auth.uid, provider: auth.provider, name: auth.info.name).first
 
     unless user
       user = User.create(
         uid: auth.uid,
-        provider: auth.provider
+        provider: auth.provider,
+        name: auth.info.name
       )
     end
     user
